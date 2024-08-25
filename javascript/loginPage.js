958dee34b8759efaa8cc9ecb9b9a7746c7025d3f
LABiB-1 <najibmahmudlabib.1@gmail.com>
2024-08-25 20:24:00 +0600

    wallet.html file complete

--- javascript/loginPage.js
+++ javascript/loginPage.js
@@ -1,15 +1,15 @@
 document.getElementById('login-btn').addEventListener('click', function(){
-    event.preventDefault();
+    event.preventDefault(); 
     const userEmail = document.getElementById('user-email');
     const email = userEmail.value;
 
     const userPass = document.getElementById('user-pass');
     const password = userPass.value;
     if(email === 'najib@gmail.com' && password === '12345678'){
-        console.log('valid user')
+        window.location.href = 'wallet.html';
     }
     else{
-        console.log('invalid user')
+        alert('incorrect email or password')
     }
 
 })
